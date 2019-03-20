8451fb58bff88d35c08f89a2114143d5bd9d0f72
Mpatadia <mrunalhpatadia@gmail.com>
2019-03-20 17:07:35 +0530

    thrid commit

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('./keys');
-
+//const User = require('../models/user-model');
 passport.use( //use middelware
     new GoogleStrategy({    
         callbackURL:'auth/google/redirect',
@@ -9,7 +9,10 @@ passport.use( //use middelware
         clientSecret:keys.google.clientSecret
     },(accessToken,refreshToken,profile,done)=>{
         console.log('passport callback funciton');
-        console.log(profile);  
+        console.log(profile); 
+        var username= profile.displayName;
+        var googleid= profile.id;
+        
     })
 )
 //Strategy diffrent ways to login
